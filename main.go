c8154008b3dc70607626b945c2c87c643cc81123
nguyenthuan2704 <nguyenthuan2704@gmail.com>
2023-09-15 15:03:48 +0700

    added middleware

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
 	"net/http"
 	"os"
+	"todolist-clean-architecture/common"
+	"todolist-clean-architecture/middleware"
 	ginitem "todolist-clean-architecture/modules/item/transport/gin"
 )
 
@@ -19,11 +22,25 @@ func main() {
 	}
 
 	r := gin.Default()
+	r.Use(middleware.Recovery())
+	/*
+		Có 3 cách sử dụng Middleware
+		Cách 1 :
+		r.Use(middleware.Recovery())
+	*/
 
 	v1 := r.Group("/v1")
+	/*
+		Cách 2:
+		v1 := r.Group("/v1",middleware.Recovery())
+	*/
 	{
 		items := v1.Group("/items")
 		{
+			/*
+				Cách 3:
+				items.POST("", middleware.Recovery(), ginitem.CreateItem(db))
+			*/
 			items.POST("", ginitem.CreateItem(db))
 			items.GET("", ginitem.ListItem(db))
 			items.GET("/:id", ginitem.GetItem(db))
@@ -33,6 +50,11 @@ func main() {
 	}
 
 	r.GET("/ping", func(c *gin.Context) {
+		/*fmt.Println([]int{}[0])*/
+		go func() {
+			defer common.Recovery()
+			fmt.Println([]int{}[0])
+		}()
 		c.JSON(http.StatusOK, gin.H{
 			"message": "pong",
 		})
